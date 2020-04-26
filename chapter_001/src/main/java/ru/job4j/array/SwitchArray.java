3502b3586cdc3284858c3539232cd6c27a6bf6bd
ViacheslavKov <slavakov92@gmail.com>
2020-04-27 02:01:22 +1000

    6.2.3. Перевернуть массив.[#229250]

--- chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,9 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        return array;
+    }
 
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
