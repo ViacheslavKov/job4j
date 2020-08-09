d305cb7c16cfa5888f81bd49666f884ef5b50a14
ViacheslavKov <slavakov92@gmail.com>
2020-08-09 22:06:19 +1000

    6.2.2. Переставить элементы массива[226795#229252]

--- chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,7 +1,11 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+
     public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
         return array;
     }
 
@@ -20,3 +24,4 @@ public class SwitchArray {
         }
     }
 }
+
--- chapter_001/src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -1,24 +1,24 @@
 package ru.job4j.array;
 
 import org.junit.Test;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class SwitchArrayTest {
-
     @Test
-    public void whenSwap0to3() {
-        int[] input = {1, 2, 3, 4};
-        int[] expect = {4, 3, 2, 1};
-        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
-        assertThat(rsl, is(expect));
+    public void whenSwap1and3() {
+        int[] input = new int[]{1, 4, 3, 2, 5};
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        int[] rst = SwitchArray.swap(input, 1, 3);
+        assertThat(rst, is(expect));
     }
 
     @Test
-    public void whenSwap4to0() {
-        int[] input = {5, 4, 3, 2, 1};
-        int[] expect = {1, 2, 3, 4, 5};
-        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
-        assertThat(rsl, is(expect));
+    public void whenSwap4and9() {
+        int[] expect = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] input = new int[] {0, 1, 2, 3, 9, 5, 6, 7, 8, 4};
+        int[] rst = SwitchArray.swap(input, 4, input.length - 1);
+        assertThat(rst, is(expect));
     }
+
 }
